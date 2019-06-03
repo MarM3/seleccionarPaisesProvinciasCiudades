ed53d2a7fa901efcf03896373ee1ed1671db579e
Mar Mazuelas <mm.zlsmng@gmail.com>
2019-06-03 12:15:39 +0200

    No funciona en funciones, cambio

--- js/codigo.js
+++ js/codigo.js
@@ -7,11 +7,45 @@ let proElegida;
 $(document).ready(function () {
     $.getJSON("datos.json", function (resultado) {
         $("#pais").append("<option value=''>--Seleccione País--</option>");
+        $("#provincia").empty();
+        $("#ciudad").empty();
         for (let i = 0; i < resultado.Paises.length; i++) {
             $("#pais").append("<option id='" + resultado.Paises[i] + "' value='" + i +
                 "'>" + resultado.Paises[i] + "</option>");
         }
-        $("#pais").change(eligeProvincia);
+        $("#pais").change(function () {
+            $("#provincia").empty();
+            $("#ciudad").empty();
+            elegido = $("#pais").children("option:selected").val();
+            if (elegido !== "") {
+                $("#provincia").append("<option value=''>--Seleccione una provincia--</option>");
+                for (let j = 0; j < resultado.Provincias.length; j++) {
+                    $("#provincia").append("<option id='" + resultado.Provincias[elegido][j] + "' value='" + j +
+                        "'>" + resultado.Provincias[elegido][j] + "</option>");
+                }
+            } else {
+                $("#icono").show();
+                $("#icono").delay(3000);
+                $("#icono").fadeOut();
+                return false;
+            }
+            $("#provincia").change(function(){
+                $("#ciudad").empty();
+                proElegida = $("#provincia").children("option:selected").val();
+                if (elegido !== "" && proElegida !== ""){
+                    $("#ciudad").append("<option value=''>--Seleccione una ciudad--</option>");
+                    for(let k=0; k<resultado.Ciudades.length; k++){
+                        $("#ciudad").append("<option id='" + resultado.Ciudades[elegido][proElegida][k] + "' value='" + k
+                        + "'>" + resultado.Ciudades[elegido][proElegida][k] + "</option>");
+                    }
+                }else {
+                    $("#icono").show();
+                    $("#icono").delay(3000);
+                    $("#icono").fadeOut();
+                    return false;
+                }
+            });
+        });
         //$("#pais").change(cargarProvincias);
         $("#enviar").click(function () {
             if ($("#pais").val() === "" || $("#provincia").val() === "" || $("#ciudad").val() === "") {
@@ -24,36 +58,4 @@ $(document).ready(function () {
         });
     });
 });
-function eligeprovincia() {
-    $("#provincia").empty();
-    elegido = $("#pais").children("option:selected").val();
-    if (elegido !== "") {
-        $("#provincia").append("<option value=''>--Seleccione una provincia--</option>");
-        for (let j = 0; j < resultado.Provincias.length; j++) {
-            $("#provincia").append("<option id='" + resultado.Provincias[elegido][j] + "' value='" + j +
-                "'>" + resultado.Provincias[elegido][j] + "</option>");
-        }
-    } else {
-        $("#icono").show();
-        $("#icono").delay(3000);
-        $("#icono").fadeOut();
-        return false;
-    }
-    $("#provincia").change(eligeCiudad);
-};
-function eligeCiudad(){
-    $("#ciudad").empty();
-    proElegida = $("#provincia").children("option:selected").val();
-    if (elegido !== "" && proElegida !== ""){
-        $("#ciudad").append("<option value=''>--Seleccione una ciudad--</option>");
-        for(let k=0; k<resultado.Ciudades.length; k++){
-            $("#ciudad").append("<option id='" + resultado.Ciudades[elegido][proElegida][k] + "' value='" + k
-            + "'>" + resultado.Ciudades[elegido][proElegida][k] + "</option>");
-        }
-    }else {
-        $("#icono").show();
-        $("#icono").delay(3000);
-        $("#icono").fadeOut();
-        return false;
-    }
-};
+
